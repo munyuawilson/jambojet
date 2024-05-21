a7eb00d6a414fc29e718123fe51af8f3bdd082a3
Ivy Karunjigi <wmunyua4@gmail.com>
2024-05-21 12:46:34 +0300

    Done with landing page UI focus mostly on the functionality now

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jambojet || Home</title>
 
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">  
 <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">  
+
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/animate.min.css') }}">
 
@@ -100,7 +100,7 @@
 
 
     <div class="container text-center pb-5">
-    <div class="container container-bg ">
+    <div class="container container-bg pb-5 ">
     <a href="" class=" container-bg rounded pt-2 pb-2 px-3 book-flight-link"> BOOK FLIGHT</a>
     <a href="" class=" bg-light rounded pt-2 pb-2 px-3 default-color link"> CHECK IN</a>
     <a href="" class=" bg-light rounded pt-2 pb-2 px-3 default-color link"> MANAGE MY BOOKING</a>
@@ -113,10 +113,12 @@
 <label class="btn btn-light default-color px-2"><input type="radio" name="travel" id="" class="btn ">Round Trip </label>
 <label class="btn btn-light default-color px-2"><input type="radio" name="travel" id="" autocomplete="off">Multicity </label>
 </div>
-<div class="mt-3 ">
+<div class="mt-3 form-group ">
 <input type="text" value="" placeholder="From"><img src="" alt="">
 <input type="text" placeholder="To">
-
+<input type="date">
+<input type="number">
+<input type="submit" value="Search">
 </div>
 
 </form>
@@ -125,7 +127,168 @@
    
     </div>
 
+
+
+
+  <div class="container mt-3 mb-3">
+      <div class="row">
+        
+        <div class="col eldoret mx-2"> <div class="container-bg text-white justify-content-end pt-5 pb-5 px-5 eldoret-price text-center float-end">Eldoret<br>
+          <span class="flights">Flights From</span> $41</div></div>
+        <div class="col malindi mx-2"><div class="container-bg text-white justify-content-end pt-5 pb-5 px-5 eldoret-price text-center float-end">Malindi<br>
+          <span class="flights">Flights From</span> $51</div></div>
+      </div>
+  </div>
+
+
+<div class="container mt-5 mb-5">
+<div class="row ">
+<div class="col mx-2 border shadow-lg"><div class="row">
+  <div class="col"><img src="{{ asset('/images\_mombasa-1500-x-500px.jpg') }}" alt="" height="70px"></div>
+  <div class="col"> <div class="row"><div class="col mt-3">
+    Mombasa
+  </div>
+  <div class="col mt-3">
+    Flights From <br> <span class="flights">USD 41</span>
+  </div></div></div>
+</div></div>
+<div class="col border shadow-lg">
+<div class="row">
+  <div class="col"><img src="{{ asset('/images\_kisumu-1500-x-500px.jpg') }}" alt="" height="70px"></div>
+  <div class="col"> <div class="row"><div class="col mt-3">
+    Kisumu
+  </div>
+  <div class="col mt-3">
+    Flights From <br> <span class="flights">USD 71</span> 
+  </div></div></div>
+</div>
+</div>
+  </div>
+
+</div>
+
+<div class="container mt-5 mb-5">
+<div class="row ">
+<div class="col mx-2 border shadow-lg"><div class="row">
+  <div class="col"><img src="{{ asset('/images\_goma-1500-x-500px.jpg') }}" alt="" height="70px"></div>
+  <div class="col"> <div class="row"><div class="col mt-3">
+    Goma
+  </div>
+  <div class="col mt-3">
+    Flights From <br> <span class="flights">USD 63</span>
+  </div></div></div>
+</div></div>
+<div class="col border shadow-lg">
+<div class="row">
+  <div class="col"><img src="{{ asset('/images\image-8.png') }}" alt="" height="70px"></div>
+  <div class="col"> <div class="row"><div class="col mt-3">
+    Ukunda
+  </div>
+  <div class="col mt-3">
+    Flights From <br> 
+    <span class="flights">USD 69</span>
+  </div></div></div>
+</div>
+</div>
+  </div>
+
+</div>
+  
+
+
+
+<!--email updates-->
+
+
+<div class="container mt-5">
+
+<div class="row container-bg ">
+  <div class="col mt-4">
+
+  <h2 class="flights email-update-h3 px-3">Get the latest sales straight to your email</h2>
+  
+<p class="mt-3 px-3">Do you want to be up to date? Subscribe here to receive updates on our airline, promos and customized marketing communication.</p>
+</div>
+  <div class="col mt-4 justify-content-center ">
+    <form action="" class="form">
+
+  <div class="form-group float-center pt-3">
+
+<input type="email" > 
+<input type="submit" value="Subscribe" class="text-black rounded subscribe">
+  </div>
+  
+    </form>
+    <p class="mt-3">By signing up, you agree to Jambojet's <span><a href="">Privacy Policy</a></span> and Terms of Use</p>
+  </div>
+</div>
+
+
+
+
+
+<!--footer-->
+
+<div class="container-fluid mt-5 border-top">
+  <div class="row mt-3">
+
+  <div class="col"><img src="{{ asset('/images/logo.svg') }}" height="50px"  alt="logo" ></div>
+  <div class="col">
+    <h3 class="default-color pt-2">ABOUT JAMBOJET</h3>
     
+
+
+
+
+
+
+
+  <ul class="links" >
+    <li><a href="#" class="links">Our story</a></li>
+    <li><a href="#" class="links">Press Room</a></li>
+    <li><a href="#" class="links">Advertise with Jambojet</a></li>
+    <li><a href="#" class="links">Careers</a></li>
+    <li><a href="#" class="links">Jambojet Sustainability</a></li>
+    <li><a href="#" class="links">On Time Performance</a></li>
+    <li><a href="#" class="links">Our Fleet</a></li>
+    <li><a href="#" class="links">Offices</a></li>
+</ul>
+  </div>
+  <div class="col">
+    <h3 class="default-color pt-2">OUR PRODUCTS</h3>
+  <ul class="links">
+    <li><a href="#" class="links">Jambojet Cargo</a></li>
+    <li><a href="#" class="links">Jambojet Rewards</a></li>
+    <li><a href="#"class="links">Jambojet Charters</a></li>
+    <li><a href="#" class="links">Jambojet Shukrani</a></li>
+    <li><a href="#" class="links">Jaza Pole Pole</a></li>
+    <li><a href="#" class="links">Jambojet Groups</a></li>
+</ul>
+  </div>
+  <div class="col">
+<h3 class="default-color pt-2">SELF CARE/SUPPORT</h3>
+  <ul class="links">
+    <li><a href="#" class="links">Manage Booking</a></li>
+    <li><a href="#" class="links">Online check-in</a></li>
+    <li><a href="#" class="links">Refund Form</a></li>
+    <li><a href="#" class="links">Business Partner</a></li>
+    <li><a href="#" class="links">FAQs</a></li>
+    <li><a href="#" class="links">Contact us</a></li>
+    <li><a href="#" class="links">Terms and Conditions</a></li>
+    <li><a href="#" class="links">Privacy Policy</a></li>
+    <li><a href="#" class="links">Jambojet Connect (Intranet)</a></li>
+</ul>
+  </div>
+  </div>
+</div>
+</div>
+    
+
+
+
+
+
+
 </body>
 <script src="{{ asset('/js/index.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
